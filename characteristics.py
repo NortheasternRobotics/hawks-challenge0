15e35073e9f53c15ba7d0ec651506f78670d07a1
wdecramer <wcdecramer@gmail.com>
2017-10-14 16:56:55 -0400

    Initial Commit

--- characteristics.py
+++ characteristics.py
@@ -1,3 +1,17 @@
+#store different characteristics of a water bottle and then aggregate the values into a decision
+#eventually combine multiple characteristics to determine percent confidence that the object is a water bottle
 class Characteristics():
 
-    def __init__(self, cap, shape):
+    def __init__(self):
+        self.cap = 0    #try to detect a cap
+        self.shape = 0  #try to detect a water bottle like shape
+
+
+    def set_cap(self, cap_value):
+        self.cap = cap_value
+
+    def set_shape(self, shape_value):
+        self.shape = shape_value
+
+
+
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from __future__ import print_function
 from PIL import Image
 from PIL import ImageFilter
+from characteristics import Characteristics
 
 
 directory_size = 1
-#read in images
+#read in images from data set
 for i in range(0, directory_size):
-    im = Image.open('pic' + str(i) + '.jpg')
+    im = Image.open('data\pic' + str(i) + '.jpg')
     im.show()
+    im1 = im.filter(ImageFilter.EDGE_ENHANCE)  # can be used to enhance the edges of shapes
+    im1.show()
+
+Characteristics.set_cap(100)
 
 #print(im.size, im.format, im.mode)
-#im1 = im.filter(ImageFilter.EDGE_ENHANCE)
-
-#
